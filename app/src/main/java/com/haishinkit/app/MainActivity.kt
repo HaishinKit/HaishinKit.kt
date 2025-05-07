d6e97f09749a4c0898ae6c52920d72e55e4202ac
shogo4405 <shogo4405@gmail.com>
2025-05-07 23:34:47 +0900

    enableEdgeToEdge()

--- app/src/main/java/com/haishinkit/app/MainActivity.kt
+++ app/src/main/java/com/haishinkit/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.haishinkit.app
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,6 +15,7 @@ class MainActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val navigation = findViewById<BottomNavigationView>(R.id.navigation)
