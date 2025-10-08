fa625bc63a44cf2ed9b304cabe094429b8db8730
ParulVermaVT <148210649+ParulVermaVT@users.noreply.github.com>
2025-10-09 01:18:42 +0530

    Added tags

--- rtmp/build.gradle.kts
+++ rtmp/build.gradle.kts
@@ -31,6 +31,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 afterEvaluate {
