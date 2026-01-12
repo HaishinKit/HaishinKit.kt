c43ba190aa1abf9e578e881ae77dd3a730c32a72
shogo4405 <shogo4405@gmail.com>
2026-01-13 01:08:42 +0900

    Update kotlin 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+            )
+        }
+    }
     buildFeatures { compose = true }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
