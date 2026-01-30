ec553941c8d2e3d9959cd1c1b74408b587594b64
shogo4405 <shogo4405@gmail.com>
2026-01-30 09:27:34 +0900

    Add KDoc comment.

--- haishinkit/src/main/java/com/haishinkit/screen/scene/Scene.kt
+++ haishinkit/src/main/java/com/haishinkit/screen/scene/Scene.kt
@@ -2,6 +2,18 @@ package com.haishinkit.screen.scene
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a scene configuration.
+ *
+ * A scene groups a named screen state that can be serialized and restored.
+ * It is typically used to save, load, or switch between different screen layouts.
+ *
+ * @property name
+ * The display name of the scene.
+ *
+ * @property screen
+ * A snapshot of the screen state that defines the contents and layout of the scene.
+ */
 @Serializable
 data class Scene(
     val name: String,
--- haishinkit/src/main/java/com/haishinkit/screen/scene/SceneDocument.kt
+++ haishinkit/src/main/java/com/haishinkit/screen/scene/SceneDocument.kt
@@ -2,6 +2,20 @@ package com.haishinkit.screen.scene
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a document that contains multiple scenes.
+ *
+ * This class serves as the root structure for serialization and deserialization.
+ * It holds versioning information to support future format changes,
+ * along with a collection of scenes defined in the document.
+ *
+ * @property version
+ * The format version of the scene document.
+ * Used for compatibility checks and migration when loading.
+ *
+ * @property scenes
+ * A list of scenes included in this document.
+ */
 @Serializable
 data class SceneDocument(
     val version: Int,
